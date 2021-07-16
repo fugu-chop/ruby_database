d9eb18132b1784239a25161e2866c55a74e27ee1
Dean Wan <deanwan14@gmail.com>
2021-07-16 20:37:36 +1000

    Add: logic for adding, listing expenses

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE expenses (
+  id SERIAL PRIMARY KEY,
+  amount NUMERIC(6,2) NOT NULL CHECK(amount > 0.01),
+  memo TEXT NOT NULL,
+  created_on DATE NOT NULL DEFAULT NOW()
+);
+
+-- INSERT INTO expenses (amount, memo) 
+--   VALUES (14.56, 'Pencils'),
+--   (3.29, 'Coffee'),
+--   (49.99, 'Text Editor');
